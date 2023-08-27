487fe201c876801b3062aa5df2e944506da12874
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-08-27 16:04:05 +0700

    remove submodule

---
+++ lib/screen/home_chat/home_chat.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+class HomeChat extends StatefulWidget {
+  const HomeChat({super.key});
+
+  @override
+  State<HomeChat> createState() => _HomeChatState();
+}
+
+class _HomeChatState extends State<HomeChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
